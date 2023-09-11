e6fa7aac006a6522689ea48fd3ce272fbc769c06
Luis Ramirez <lramirez68@ucol.mx>
2023-09-11 10:50:34 -0600

    creamos las carpetas, usenlas, no las borren csms

---
+++ admindashboard/dashboard_admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="loginstyle.css">
+    <script src="loginscript.js" async></script>
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico.png">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+</head>
+<body>
+    
+</body>
+</html>
