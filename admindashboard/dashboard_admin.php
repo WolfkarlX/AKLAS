16f39d3c596bf593eaab1161fd42643d6837ab33
Luis Ramirez <lramirez68@ucol.mx>
2023-09-12 09:00:00 -0600

    se puso un sidebar xdxd

--- admindashboard/dashboard_admin.php
+++ admindashboard/dashboard_admin.php
@@ -5,13 +5,78 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="../css/dashboard_admin.css">
     <script src="loginscript.js" async></script>
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel="stylesheet">
 </head>
 </head>
 <body>
-    
+    <div class="sidebar">
+        <div class="top">
+            <div class="logo">
+                <i class="bx bx-cross">AKLAS</i>
+            </div>
+            <i class="bx bx-menu" id="btn_menu"></i>
+        </div>
+        <ul>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home-alt-2'></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+        </ul>
+    </div>
+    <div class="main-content">
+        <div class="arriba">
+            <span>HOLA</span>
+        </div>
+    </div>
 </body>
+<script>
+    let btn_sidebar = document.querySelector('#btn_menu'); //Guarda una variable del boton del sidebar
+    let sidebar = document.querySelector('.sidebar'); //Guarda una variable del div sidebar completo
+
+    btn_sidebar.onclick = function () { //Funcion para abrir sidebar xD
+        sidebar.classList.toggle('active'); // C abre
+    };
+</script>
 </html>
