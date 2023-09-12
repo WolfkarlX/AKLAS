75d0a5160fb4d9f468143d1739b8d5bfad8a1477
Saúl Bustamante Bernabe <sbustamante@ucol.mx>
2023-09-12 11:12:17 -0600

    Conexion a base de datos agregada

---
+++ models/conexion.php
@@ -0,0 +1,21 @@
+<?php
+    namespace models;
+    use PDO, PDOException;
+    class conexion extends PDO{
+        private $servername= "localhost";
+        private $username= "root";
+        private $password= "";
+        private $dbname= "aklas";
+
+        public function __construct()
+        {
+            try{
+                parent::__construct("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+                $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+        }
+    }
+?>
