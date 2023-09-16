a1530497a650e072649a43477af8bd9e465d0a24
Alang315 <alguienxc2@hotmail.com>
2023-09-15 21:59:57 -0600

    se eliminan categorias

--- controllers/categories.php
+++ controllers/categories.php
@@ -9,6 +9,17 @@
     $categories = $stmt ->fetchAll();
 ?>
 
+<?php 
+    if(isset($_POST["delete"])){
+        $id = intval($_POST["delete"]);
+        $sql = "DELETE FROM categories WHERE CategoryID = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        header("Location:categories.php");
+        exit();
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,15 +29,38 @@
     <title>Create</title>
 </head>
 <body>
+    <h1>Categories</h1>
     <a href="ccategory.php">Create a new category</a>
-    <h1>Aqui se imprimen todas las categorias existentes</h1>
+    
     <?php
         foreach($categories as $r){ ?>
             <h1><?php echo $r["CategoryName"]; ?></h1>
             <p><?php echo $r["Description"]; ?></p>
-            <a href="">edit</a>
-            <a href=""> delete</a>
+            <a href=" <?php $_POST["ID"] = $r["CategoryID"]; ?> ">edit</a>
+            <form action="categories.php" method="POST">
+                <button onclick="return confirmarAccion()" id="Delete" name="delete" value="<?php echo $r["CategoryID"];?>"> Delete </button> 
+            </form>
             <br>
+    
     <?php } ?> 
+
 </body>
 </html>
+<!--Script para hacer la confirmacion de eliminacion de un registro o categoria -->
+
+<script>
+    function confirmarAccion() {
+        // Utiliza la función confirm() para mostrar el mensaje de confirmación
+        var confirmacion = confirm("¿Are you sure you want to delete this categorie?"); 
+        // Comprueba la respuesta del usuario
+        if(confirmacion){
+            // El usuario hizo clic en "Aceptar", realiza la acción
+            alert("category eliminated succesfully.");
+            return true
+        }else{
+            // El usuario hizo clic en "Cancelar" o cerró la ventana de confirmación
+            alert("The action was canceled");
+            return false
+        }
+    }
+</script>
