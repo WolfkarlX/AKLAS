f9cb82c6aad7f05ad34ab0d858ab7a1dfccc9589
Saúl Bustamante Bernabe <sbustamante@ucol.mx>
2023-09-17 09:06:01 -0600

    Funciones de editado y eleminado completadas La clase de proveedor cuenta con los metodos de editar y eleminars

--- models/supplier.php
+++ models/supplier.php
@@ -1,7 +1,7 @@
 <?php
 namespace models;
 
-class supplier extends conexion{ #Clase de proveedor que extiende la conexion, aqui se encontraran las funciones para manipular la tabla de proveedores
+class supplier extends conexion{ //Clase de proveedor que extiende la conexion, aqui se encontraran las funciones para manipular la tabla de proveedores
     private $id;
     private $name;
     private $contact;
@@ -18,6 +18,7 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         $this->table = "suppliers";
     }
 
+    //Agrega un provedores a la tabla de la base de datos
     public function addSupplier($SupplierName=null, $ContactName=null, $Address=null, $City=null, $PostalCode=null, $Country=null, $Phone=null){
         // Preparar una sentencia sql para insertar datos
         $sql = "INSERT INTO suppliers ( SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES ( :SupplierName, :ContactName, :Address, :City, :PostalCode, :Country, :Phone)";
@@ -36,6 +37,7 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         return $stmt->execute();
     }
 
+    //Devuleve todos los proveedores
     public function getSuppliers(){
         $sql = "SELECT * FROM {$this->table}";
         $stmt = $this->prepare($sql);
@@ -43,6 +45,7 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         return $stmt->fetchAll();
     }
 
+    //Devuelve un proveedor segun su id
     public function getSuppliersByID($id){
         $sql = "SELECT * FROM {$this->table} WHERE SupplierID = :SupplierID";
         $stmt = $this->prepare($sql);
@@ -51,6 +54,7 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         return $stmt->fetch();
     }
 
+    //Edita un proveedor segun el id, se especifica el valor del campon en orden de la tabla, en caso de null se toma el valor actual de la tabla
     public function editSupplier($id, $SupplierName=null, $ContactName=null, $Address=null, $City=null, $PostalCode=null, $Country=null, $Phone=null){
         // Preparar una sentencia sql para editar datos
         $sql = "UPDATE suppliers SET SupplierName = :SupplierName, ContactName = :ContactName, Address = :Address, City = :City, PostalCode = :PostalCode, Country = :Country, Phone = :Phone WHERE SupplierID = :SupplierID";
@@ -58,7 +62,7 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         $stmt = $this->prepare($sql);
 
         // Asignar valores a los parámetros con bindParam
-        $stmt->bindParam(':SupplierID', $SupplierID);
+        $stmt->bindParam(':SupplierID', $id);
         $stmt->bindParam(':SupplierName', $SupplierName);
         $stmt->bindParam(':ContactName', $ContactName);
         $stmt->bindParam(':Address', $Address);
@@ -70,13 +74,27 @@ class supplier extends conexion{ #Clase de proveedor que extiende la conexion, a
         // Establecer el valor anterior en case de ser nulo
         $oldData = $this->getSuppliersByID($id);
         if(is_null($SupplierName)) $SupplierName = $oldData[1];
-        if(is_null($ContactName)) $SupplierName = $oldData[2];
-        if(is_null($Address)) $SupplierName = $oldData[3];
-        if(is_null($City)) $SupplierName = $oldData[4];
-        if(is_null($PostalCode)) $SupplierName = $oldData[5];
-        if(is_null($Country)) $SupplierName = $oldData[6];
+        if(is_null($ContactName)) $ContactName = $oldData[2];
+        if(is_null($Address)) $Address = $oldData[3];
+        if(is_null($City)) $City = $oldData[4];
+        if(is_null($PostalCode)) $PostalCode = $oldData[5];
+        if(is_null($Country)) $Country = $oldData[6];
         if(is_null($Phone)) $SupplierName = $oldData[7];
 
+        return $stmt->execute();
+    }
+
+    //Elimina un proveedor segun su id
+    public function delSupplier($id){
+        // Preparar una sentencia sql para borrar datos
+        $sql = "DELETE FROM suppliers WHERE SupplierID = :SupplierID";
+
+        $stmt = $this->prepare($sql);
+
+        // Se asigna el id del registro
+        $stmt->bindParam(':SupplierID', $id);
+
+        // Ejecuta la sentencia
         return $stmt->execute();
     }
 }
