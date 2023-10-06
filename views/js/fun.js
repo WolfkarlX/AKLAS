3cbac2f6247692c032027fda61a2889e71905a5b
Saúl Bustamante Bernabe <sbustamante@ucol.mx>
2023-10-06 11:27:42 -0600

    El Sidebar mantiene su estado en cualquier pagina

--- views/js/fun.js
+++ views/js/fun.js
@@ -1,20 +1,31 @@
 let btn_sidebar = document.querySelector('#btn_menu'); //Guarda una variable del boton del sidebar
 let sidebar = document.querySelector('.sidebar'); //Guarda una variable del div sidebar completo
+
 let btn_registro = document.querySelector("#btn-registro");//Boton para abrir formulario
 let btn_cerrarform = document.querySelector("#Cerrar_form");//Boton X para cerrarlo
 let btn_cancelarform = document.querySelector("#Cancelar_registro");
 let btn_submitform = document.querySelector("#submit");
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
-
 btn_sidebar.onclick = function () { //Funcion para abrir sidebar xD
     sidebar.classList.toggle('active'); // C abre
+    localStorage.setItem("sidebar", sidebar.classList.contains('active'));
 };
-btn_registro.onclick = function showForm() {
-  document.getElementById("myForm").style.display = "block";
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("sidebar") === "true") {
+    sidebar.classList.toggle('active');
+  }
+});
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
-btn_cerrarform.onclick = closeForm;
-btn_cancelarform.onclick = closeForm;
-btn_submitform.onclick = closeForm;
+
+if (btn_registro) {
+  btn_registro.onclick = function showForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  btn_cerrarform.onclick = closeForm;
+  btn_cancelarform.onclick = closeForm;
+  btn_submitform.onclick = closeForm;
+}
