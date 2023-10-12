822daf6e468ff457d3a127a97ba3dc18c33220a6
luis <lramirez68@ucol.mx>
2023-10-12 12:00:59 -0600

    mas css

--- views/js/loginfun.js
+++ views/js/loginfun.js
@@ -7,6 +7,6 @@ icono.onclick = togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
     let type = contra.getAttribute('type') === 'password' ? 'text' : 'password';
     contra.setAttribute('type', type);
-    // toggle the eye slash icon
+    // icono
     togglePassword.classList.toggle("bx-lock-open-alt");
   });
