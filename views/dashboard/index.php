297f41c730b92810428c3dad52ff50b3fe341dfb
Saúl Bustamante Bernabe <sbustamante@ucol.mx>
2023-11-20 16:32:24 -0600

    Se muestra el dashboard Dispercion de los productos en las areas

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -168,7 +168,7 @@
                 data:{
                     datasets: [{
                         label: 'Stock de Productos',
-                        backgroundColor: ['#6bf1ab','#63d69f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1'],
+                        backgroundColor: ['#6bf1ab','#93f64f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1'],
                         borderColor: ['black'],
                         borderWidth:1
                     }]
@@ -182,7 +182,7 @@
                 }
             })
 
-            let url = 'http://localhost/apirest/articulos.php'
+            let url = '../../controllers/each-area-dashboard.php';
             fetch(url)
                 .then( response => response.json() )
                 .then( datos => mostrar(datos) )
@@ -191,8 +191,8 @@
 
             const mostrar = (articulos) =>{
                 articulos.forEach(element => {
-                    myChart.data['labels'].push(element.descripcion)
-                    myChart.data['datasets'][0].data.push(element.stock)
+                    myChart.data['labels'].push(element.area_nombre)
+                    myChart.data['datasets'][0].data.push(element.porcentaje)
                     myChart.update()
                 });
                 console.log(myChart.data)
